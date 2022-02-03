90efb7371643b8f52908a7457f7f6d7f173c7a74
Alexander R. Torrijos <artorrijos@gmail.com>
2022-02-03 18:53:30 +0800

    Initial commit

---
+++ ArrayAccessLib/ArrayAccess.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace ArrayAccessLib;
+
+public class ArrayAccess
+{
+    private readonly int[] _sampleArray = new int[] { 1, 2 };
+
+    public int AccessNonExistingIndex()
+    {
+        return _sampleArray[2];
+    }
+}
---
+++ ArrayAccessLibTests/ArrayAccessTests.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using ArrayAccessLib;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ArrayAccessLibTests;
+
+public class ArrayAccessTests
+{
+    private readonly ITestOutputHelper _output;
+
+    public ArrayAccessTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void TestArrayAccessOutofBounds()
+    {
+        // Arrange
+        var sut = new ArrayAccess();
+
+        // Act
+        // var result = sut.AccessNonExistingIndex();
+
+        // Assert
+        var ex = Assert.Throws<InvalidOperationException>(() => sut.AccessNonExistingIndex());
+
+        _output.WriteLine($"Exception: {ex.Message}");
+    }
+}
